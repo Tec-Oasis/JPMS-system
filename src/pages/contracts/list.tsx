7040c6d5078edb9b915f474cb73bbe92529ec835
buudi <buudi.dev@gmail.com>
2023-12-28 19:55:37 +0800

    Update package.json and show.tsx

--- src/pages/contracts/list.tsx
+++ src/pages/contracts/list.tsx
@@ -4,6 +4,8 @@ import {
   BaseRecord,
   useTranslate,
   useMany,
+  CrudFilters,
+  HttpError,
 } from "@refinedev/core";
 import {
   useTable,
@@ -12,11 +14,60 @@ import {
   DateField,
   BooleanField,
 } from "@refinedev/antd";
-import { Table, Space } from "antd";
+
+import { SearchOutlined } from "@ant-design/icons";
+import {
+  Button,
+  Table,
+  Space,
+  Row,
+  Col,
+  Card,
+  Form,
+  Input,
+  Select,
+  DatePicker,
+  FormProps,
+} from "antd";
+
+interface IContract {
+  id: number;
+  tenant_id: number;
+  property_id: number;
+  contract_start: string;
+  contract_end: string;
+  rent: number;
+  active: boolean;
+}
 
 export const ContractList: React.FC<IResourceComponentsProps> = () => {
   const translate = useTranslate();
-  const { tableProps } = useTable({
+
+  const { tableProps, searchFormProps } = useTable<IContract>({
+    initialSorter: [
+      {
+        field: "id",
+        order: "desc",
+      },
+    ],
+    onSearch: (params: any) => {
+      const filters: CrudFilters = [];
+      const { q, active } = params;
+      filters.push({
+        field: "q",
+        operator: "eq",
+        value: q,
+      });
+
+      filters.push({
+        field: "active",
+        operator: "eq",
+        value: active,
+      });
+
+      return filters;
+    },
+
     syncWithLocation: true,
   });
 
@@ -37,50 +88,162 @@ export const ContractList: React.FC<IResourceComponentsProps> = () => {
   });
 
   return (
-    <List>
-      <Table {...tableProps} rowKey="id">
-        <Table.Column
-          dataIndex={["contract_start"]}
-          title={translate("contracts.fields.contract_start")}
-          render={(value: any) => <DateField value={value} />}
-        />
-        <Table.Column
-          dataIndex={["contract_end"]}
-          title={translate("contracts.fields.contract_end")}
-          render={(value: any) => <DateField value={value} />}
-        />
-        <Table.Column
-          dataIndex="rent"
-          title={translate("contracts.fields.rent")}
-        />
-        <Table.Column
-          dataIndex="notes"
-          title={translate("contracts.fields.notes")}
-        />
-        <Table.Column
-          dataIndex={["active"]}
-          title={translate("contracts.fields.active")}
-          render={(value: any) => <BooleanField value={value} />}
-        />
-        <Table.Column
-          dataIndex={["property_id"]}
-          title={translate("contracts.fields.property_id")}
-        />
-        <Table.Column
-          dataIndex={["tenant_id"]}
-          title={translate("contracts.fields.tenant_id")}
-        />
-        <Table.Column dataIndex="id" title={translate("contracts.fields.id")} />
-        <Table.Column
-          title={translate("table.actions")}
-          dataIndex="actions"
-          render={(_, record: BaseRecord) => (
-            <Space>
-              <ShowButton hideText size="small" recordItemId={record.id} />
-            </Space>
-          )}
-        />
-      </Table>
-    </List>
+    <Row gutter={[16, 16]}>
+      <Col
+        xl={6}
+        lg={24}
+        xs={24}
+        style={{
+          marginTop: "48px",
+        }}
+      >
+        <Card title={"Filters"}>
+          <Filter formProps={searchFormProps} />
+        </Card>
+      </Col>
+      <Col xl={18} xs={24}>
+        <List>
+          <Table {...tableProps} rowKey="id">
+            <Table.Column
+              dataIndex={["tenant_id"]}
+              title={translate("contracts.fields.fname")}
+              render={(value: any) => {
+                const tenant = tenantData?.data?.find(
+                  (item) => item.id === value
+                );
+                return tenant?.fname;
+              }}
+            />
+            <Table.Column
+              dataIndex={["tenant_id"]}
+              title={translate("contracts.fields.lname")}
+              render={(value: any) => {
+                const tenant = tenantData?.data?.find(
+                  (item) => item.id === value
+                );
+                return tenant?.lname;
+              }}
+            />
+            <Table.Column
+              dataIndex={["property_id"]}
+              title={translate("contracts.fields.property_id")}
+              render={(value: any) => {
+                const property = propertyData?.data?.find(
+                  (item) => item.id === value
+                );
+                return property?.name;
+              }}
+            />
+            <Table.Column
+              dataIndex={["contract_start"]}
+              title={translate("contracts.fields.contract_start")}
+              render={(value: any) => <DateField value={value} />}
+            />
+            <Table.Column
+              dataIndex={["contract_end"]}
+              title={translate("contracts.fields.contract_end")}
+              render={(value: any) => <DateField value={value} />}
+            />
+            <Table.Column
+              dataIndex="rent"
+              title={translate("contracts.fields.rent")}
+            />
+            {/* <Table.Column
+              dataIndex="notes"
+              title={translate("contracts.fields.notes")}
+            /> */}
+            <Table.Column<IContract>
+              key="active"
+              dataIndex="active"
+              title={translate("contracts.fields.active")}
+              render={(value: any) => <BooleanField value={value} />}
+            />
+
+            {/* <Table.Column
+              dataIndex="id"
+              title={translate("contracts.fields.id")}
+            /> */}
+            <Table.Column
+              title={translate("table.actions")}
+              dataIndex="actions"
+              render={(_, record: BaseRecord) => (
+                <Space>
+                  <ShowButton hideText size="small" recordItemId={record.id} />
+                </Space>
+              )}
+            />
+          </Table>
+        </List>
+      </Col>
+    </Row>
+  );
+};
+
+const Filter: React.FC<{ formProps: FormProps }> = (props) => {
+  const t = useTranslate();
+
+  const { RangePicker } = DatePicker;
+
+  return (
+    <Form layout="vertical" {...props.formProps}>
+      <Row gutter={[10, 0]} align="bottom">
+        <Col xs={24} xl={24} md={12}>
+          <Form.Item label={t("users.filter.search.label")} name="q">
+            <Input
+              placeholder={t("users.filter.search.placeholder")}
+              prefix={<SearchOutlined />}
+            />
+          </Form.Item>
+        </Col>
+        {/* <Col xs={24} xl={24} md={12}>
+          <Form.Item label={t("users.filter.createdAt.label")} name="createdAt">
+            <RangePicker style={{ width: "100%" }} />
+          </Form.Item>
+        </Col> */}
+        {/* <Col xs={24} xl={24} md={8}>
+          <Form.Item label={t("users.filter.gender.label")} name="gender">
+            <Select
+              allowClear
+              placeholder={t("users.filter.gender.placeholder")}
+              options={[
+                {
+                  label: t("users.filter.gender.male"),
+                  value: "Male",
+                },
+                {
+                  label: t("users.filter.gender.female"),
+                  value: "Female",
+                },
+              ]}
+            />
+          </Form.Item>
+        </Col> */}
+        <Col xs={24} xl={24} md={8}>
+          <Form.Item label={t("users.filter.active.label")} name="active">
+            <Select
+              allowClear
+              placeholder={t("users.filter.active.placeholder")}
+              options={[
+                {
+                  label: t("users.filter.active.true"),
+                  value: "true",
+                },
+                {
+                  label: t("users.filter.active.false"),
+                  value: "false",
+                },
+              ]}
+            />
+          </Form.Item>
+        </Col>
+        <Col xs={24} xl={24} md={8}>
+          <Form.Item>
+            <Button style={{ width: "100%" }} htmlType="submit" type="primary">
+              {t("users.filter.submit")}
+            </Button>
+          </Form.Item>
+        </Col>
+      </Row>
+    </Form>
   );
 };
--- src/pages/contracts/show.tsx
+++ src/pages/contracts/show.tsx
@@ -13,7 +13,28 @@ import {
   TextField,
   BooleanField,
 } from "@refinedev/antd";
-import { Typography } from "antd";
+import {
+  Typography,
+  Row,
+  Col,
+  Card,
+  Spin,
+  Progress,
+  Flex,
+  Image,
+  Descriptions,
+} from "antd";
+import {
+  UserOutlined,
+  HomeOutlined,
+  CalendarOutlined,
+  DollarOutlined,
+  FileTextOutlined,
+  CheckCircleOutlined,
+  AppstoreAddOutlined,
+  UnorderedListOutlined,
+} from "@ant-design/icons";
+import moment from "moment";
 
 const { Title } = Typography;
 
@@ -40,24 +61,157 @@ export const ContractShow: React.FC<IResourceComponentsProps> = () => {
     },
   });
 
+  const {
+    img: propertyImg,
+    name: propertyName,
+    description: propertyDescription,
+    rent: propertyRent,
+    property_type: propertyType,
+    total_rooms: totalRooms,
+  } = propertyData?.data || {};
+
+  const {
+    fname: tenantFname,
+    lname: tenantLname,
+    ic_no: tenantIcNo,
+    phone_country_code: tenantPhoneCountryCode,
+    phone_number: tenantPhoneNumber,
+    email: tenantEmail,
+  } = tenantData?.data || {};
+
   return (
+    // <Show isLoading={isLoading}>
+    //   <Title level={5}>{translate("contracts.fields.contract_start")}</Title>
+    //   <DateField value={record?.contract_start} />
+    //   <Title level={5}>{translate("contracts.fields.contract_end")}</Title>
+    //   <DateField value={record?.contract_end} />
+    //   <Title level={5}>{translate("contracts.fields.rent")}</Title>
+    //   <NumberField value={record?.rent ?? ""} />
+    //   <Title level={5}>{translate("contracts.fields.notes")}</Title>
+    //   <TextField value={record?.notes} />
+    //   <Title level={5}>{translate("contracts.fields.active")}</Title>
+    //   <BooleanField value={record?.active} />
+    //   <Title level={5}>{translate("contracts.fields.property_id")}</Title>
+    //   {propertyIsLoading ? <>Loading...</> : <>{propertyData?.data?.id}</>}
+    //   <Title level={5}>{translate("contracts.fields.tenant_id")}</Title>
+    //   {tenantIsLoading ? <>Loading...</> : <>{tenantData?.data?.id}</>}
+    //   <Title level={5}>{translate("contracts.fields.id")}</Title>
+    //   <NumberField value={record?.id ?? ""} />
+    // </Show>
+
     <Show isLoading={isLoading}>
-      <Title level={5}>{translate("contracts.fields.contract_start")}</Title>
-      <DateField value={record?.contract_start} />
-      <Title level={5}>{translate("contracts.fields.contract_end")}</Title>
-      <DateField value={record?.contract_end} />
-      <Title level={5}>{translate("contracts.fields.rent")}</Title>
-      <NumberField value={record?.rent ?? ""} />
-      <Title level={5}>{translate("contracts.fields.notes")}</Title>
-      <TextField value={record?.notes} />
-      <Title level={5}>{translate("contracts.fields.active")}</Title>
-      <BooleanField value={record?.active} />
-      <Title level={5}>{translate("contracts.fields.property_id")}</Title>
-      {propertyIsLoading ? <>Loading...</> : <>{propertyData?.data?.id}</>}
-      <Title level={5}>{translate("contracts.fields.tenant_id")}</Title>
-      {tenantIsLoading ? <>Loading...</> : <>{tenantData?.data?.id}</>}
-      <Title level={5}>{translate("contracts.fields.id")}</Title>
-      <NumberField value={record?.id ?? ""} />
+      <Flex>
+        <Card
+          title={translate("contracts.fields.property_id")}
+          style={{ margin: "10px", padding: "10px" }}
+        >
+          {propertyIsLoading ? (
+            <Spin />
+          ) : (
+            <>
+              <Image width={200} src={propertyImg} alt={propertyName} />
+              <Descriptions column={1} style={{ marginTop: "1rem" }}>
+                <Descriptions.Item label={<HomeOutlined />}>
+                  {propertyName}
+                </Descriptions.Item>
+                <Descriptions.Item label={<DollarOutlined />}>
+                  {propertyRent}
+                </Descriptions.Item>
+                <Descriptions.Item label={<AppstoreAddOutlined />}>
+                  {propertyType}
+                </Descriptions.Item>
+                <Descriptions.Item label={"rooms"}>
+                  {totalRooms}
+                </Descriptions.Item>
+              </Descriptions>
+            </>
+          )}
+        </Card>
+        <Flex style={{ marginLeft: "2rem" }}>
+          <Card
+            title={translate("contracts.fields.tenant_id")}
+            style={{ margin: "10px" }}
+          >
+            {tenantIsLoading ? <>Loading...</> : <>{tenantData?.data?.id}</>}
+          </Card>
+          <Card
+            title={translate("contracts.fields.tenant_id")}
+            style={{ margin: "10px", padding: "10px" }}
+          >
+            {tenantIsLoading ? (
+              <Spin />
+            ) : (
+              <Descriptions column={1}>
+                <Descriptions.Item label="First Name">
+                  {tenantFname}
+                </Descriptions.Item>
+                <Descriptions.Item label="Last Name">
+                  {tenantLname}
+                </Descriptions.Item>
+                <Descriptions.Item label="IC Number">
+                  {tenantIcNo}
+                </Descriptions.Item>
+                <Descriptions.Item label="Phone Country Code">
+                  {tenantPhoneCountryCode}
+                </Descriptions.Item>
+                <Descriptions.Item label="Phone Number">
+                  {tenantPhoneNumber}
+                </Descriptions.Item>
+                <Descriptions.Item label="Email">
+                  {tenantEmail}
+                </Descriptions.Item>
+              </Descriptions>
+            )}
+          </Card>
+          <Card
+            title={
+              <>
+                <CalendarOutlined /> Contract Duration
+              </>
+            }
+            style={{ margin: "10px" }}
+          >
+            <Title level={5}>
+              {translate("contracts.fields.contract_start")}
+            </Title>
+            <DateField value={record?.contract_start} />
+            <Title level={5}>
+              {translate("contracts.fields.contract_end")}
+            </Title>
+            <DateField value={record?.contract_end} />
+          </Card>
+
+          <Card
+            title={
+              <>
+                <FileTextOutlined /> Contract Details
+              </>
+            }
+            style={{ margin: "10px" }}
+          >
+            <Title level={5}>{translate("contracts.fields.rent")}</Title>
+            <NumberField value={record?.rent ?? ""} />
+            <Title level={5}>{translate("contracts.fields.notes")}</Title>
+            <TextField value={record?.notes} />
+            <Title level={5}>{translate("contracts.fields.active")}</Title>
+            <BooleanField value={record?.active} />
+          </Card>
+
+          <Card
+            title={
+              <>
+                <UserOutlined /> Associated Entities
+              </>
+            }
+            style={{ margin: "10px" }}
+          >
+            <Title level={5}>{translate("contracts.fields.property_id")}</Title>
+            {propertyIsLoading ? <Spin /> : <>{propertyData?.data?.name}</>}
+            <Title level={5}>{translate("contracts.fields.tenant_id")}</Title>
+            {tenantIsLoading ? <Spin /> : <>{tenantData?.data?.name}</>}
+          </Card>
+        </Flex>
+      </Flex>
     </Show>
   );
 };
