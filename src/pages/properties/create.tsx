e35d88d5ed756c161ee4843cd7a324d0f2f51eaa
Sakif098 <sakifhossain098@gmail.com>
2024-01-15 12:23:09 +0800

    added map to add coordinates

--- src/pages/properties/create.tsx
+++ src/pages/properties/create.tsx
@@ -1,7 +1,9 @@
-import React from "react";
+import React ,{ useState } from "react";
 import { IResourceComponentsProps, useTranslate } from "@refinedev/core";
 import { Create, useForm, useSelect } from "@refinedev/antd";
-import { Form, Input, Select, DatePicker } from "antd";
+import { Form, Input, Select, DatePicker, AutoComplete } from "antd";
+import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api";
+
 import dayjs from "dayjs";
 
 import AmenityList from "../../components/properties/AmenityList";
@@ -19,6 +21,28 @@ export const PropertyCreate: React.FC<IResourceComponentsProps> = () => {
     // console.log("Parent component onChange:", amenities);
     return amenities;
   };
+  const containerStyle = {
+    width: "100%",
+    height: "300px",
+  };
+  
+  const [center, setCenter] = useState({ lat: 25, lng: 55 });
+   
+  const [coordinates, setCoordinates] = useState({ lat: 0, lng: 0 });
+  
+
+
+  const handleMapClick = (event: google.maps.MouseEvent) => {
+  const lat = event.latLng.lat();
+  const lng = event.latLng.lng();
+
+  setCoordinates({ lat, lng });
+    setCenter({ lat, lng });
+  formProps.form?.setFieldsValue({
+    coordinate_x: lat,
+    coordinate_y: lng,
+  });
+};
 
   return (
     <Create saveButtonProps={saveButtonProps}>
@@ -56,28 +80,55 @@ export const PropertyCreate: React.FC<IResourceComponentsProps> = () => {
         >
           <Input />
         </Form.Item>
-        <Form.Item
-          label={translate("properties.fields.coordinate_x")}
-          name={["coordinate_x"]}
-          rules={[
-            {
-              required: true,
-            },
-          ]}
-        >
-          <Input />
-        </Form.Item>
-        <Form.Item
-          label={translate("properties.fields.coordinate_y")}
-          name={["coordinate_y"]}
-          rules={[
-            {
-              required: true,
-            },
-          ]}
-        >
-          <Input />
-        </Form.Item>
+        <Form.Item label="Coordinates">
+  <AutoComplete
+    value={`${coordinates.lat}, ${coordinates.lng}`}
+    onChange={(value) => {
+      formProps.form?.setFieldsValue({ coordinates: value });
+    }}
+  >
+    <Input disabled />
+  </AutoComplete>
+</Form.Item>
+
+<Form.Item
+  label={translate("properties.fields.coordinate_x")}
+  name={["coordinate_x"]}
+  rules={[
+    {
+      required: true,
+    },
+  ]}
+>
+  <Input value={coordinates.lat} disabled />
+</Form.Item>
+
+<Form.Item
+  label={translate("properties.fields.coordinate_y")}
+  name={["coordinate_y"]}
+  rules={[
+    {
+      required: true,
+    },
+  ]}
+>
+  <Input value={coordinates.lng} disabled />
+
+</Form.Item>
+
+          <LoadScript googleMapsApiKey="AIzaSyDVxOGQBiyLGzauNNOSkcnIm7Q3MjPo6Hc">
+            <GoogleMap
+              mapContainerStyle={containerStyle}
+              center={center}
+              zoom={15}
+              onClick={handleMapClick}
+            >
+              {coordinates.lat !== 0 && coordinates.lng !== 0 && (
+                <Marker position={coordinates} />
+              )}
+            </GoogleMap>
+          </LoadScript>
+        
         <Form.Item
           label={translate("properties.fields.img")}
           name={["img"]}
